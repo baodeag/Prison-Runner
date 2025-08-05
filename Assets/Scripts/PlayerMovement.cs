576a5a7edcabd81075af1a80ff01e0510a586557
baodeag <tranvietbao0907@gmail.com>
2025-08-05 21:33:45 +0700

    Animation for climbing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,16 +9,20 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
-
+    CapsuleCollider2D myCapsuleCollider;
+    float gravityScaleAtStart;
 
     [SerializeField] float runSpeed = 5f;
-    [SerializeField] float jumpSpeed = 5f; 
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        gravityScaleAtStart = myRigidbody.gravityScale; // Store the initial gravity scale
     }
 
     // Update is called once per frame
@@ -26,6 +30,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Run();
         FlipSprite();
+        ClimbingLadder();
     }
 
     void OnMove(InputValue value)
@@ -36,12 +41,18 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
-        if(value.isPressed)
+        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) // check if the player is touching the ground
+        {
+            return; // If not touching the ground, do not allow jumping
+        }
+
+        if (value.isPressed)
         {
             myRigidbody.velocity += new Vector2(0f, jumpSpeed); // Adjust the jump force as needed
         }
-
     }
+
+
 
     void Run()
     {
@@ -49,21 +60,33 @@ public class PlayerMovement : MonoBehaviour
         myRigidbody.velocity = playerVelocity;
 
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon; // Mathf.Epsilon is a very small value to avoid floating point errors
-        if (playerHasHorizontalSpeed)
-        {
-            myAnimator.SetBool("isRunning", true);
-        }
-        else
-        {
-            myAnimator.SetBool("isRunning", false);
-        }
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed); // Set the running animation state
     }
 
     void FlipSprite()
     {
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon; 
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
         }
+    }
+
+    void ClimbingLadder()
+    {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myRigidbody.gravityScale = gravityScaleAtStart; // Restore the original gravity scale
+            myAnimator.SetBool("isClimbing", false);
+            return;
+        }
+        Vector2 climbVelocity = new Vector2(myRigidbody.velocity.x, moveInput.y * climbSpeed);
+        myRigidbody.velocity = climbVelocity;
+        myRigidbody.gravityScale = 0f; // Disable gravity while climbing
+
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isClimbing", playerHasVerticalSpeed); // Reset climbing animation state
+
     }
 }
